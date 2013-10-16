59411a227ad95a7a3399921fbfdea82678243726
Hack Reactor Students <github@hackreactor.com>
2013-10-16 15:36:59 -0700

    Initial commit

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,6 +1,39 @@
 var path = require('path');
-module.exports.datadir = path.join(__dirname, "../data/sites.txt"); // tests will need to override this.
+var url = require('url');
+var fs = require('fs');
+module.exports.datadir = path.resolve(__dirname, "../data/sites.txt"); // tests will need to override this.
+
+var headers = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept, X-Requested-With",
+  "access-control-max-age": 10, // Seconds.
+  'Content-Type': "text/html"
+};
 
 module.exports.handleRequest = function (req, res) {
-  console.log(exports.datadir);
+  var pathname = url.parse(req.url).pathname;
+
+  if (req.method === 'GET') {
+    if (pathname === "/") {
+      fs.readFile(__dirname + "/public/index.html", function(err, data) {
+        if (err) {
+          res.writeHead(404, headers);
+          res.end();
+        }
+        res.writeHead(200, headers);
+        res.end(data);
+      });
+    } else {
+      fs.readFile(__dirname + "/../data/sites" + pathname, function(err, data) {
+        if (err){
+          res.writeHead(404, headers);
+          res.end();
+        }
+        res.writeHead(200, headers);
+        res.end(data);
+      });
+    }
+  }
 };
+
